660d63abbfea30b4fb67fa625c2fd344e9a19056
nageetaw <nigeetawadhwani@gmail.com>
2026-07-27 21:23:35 +0200

    80. Remove Duplicates from Sorted Array II

---
+++ 80. Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,51 @@
+class Solution(object):
+    # def removeDuplicates(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: int
+    #     """
+    #     k = 2
+    #     for i in range(2 , len(nums)):
+    #         if nums[i] != nums[k-2]:
+    #             nums[i], nums[k] = nums[k], nums[i]
+    #             k+=1
+        
+    #     return k
+
+
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        j, count = 1, 1
+        for i in range(1 , len(nums)):
+            if nums[i] == nums[i-1]:
+                count +=1
+            else:
+                count =1
+            
+            if count <=2:
+                nums[j] = nums[i]
+                j+=1
+        
+        return j
+        
+
+    # The below is to remove duplicates -- MEANS max a number will only appear once
+    # In this since we init i =0 that is why we increament i before swapping
+    #  it is the same pattern
+    # plus the current pointer(the one in for loop) and swap and update the other one when current and other pointer elements are not equal.   
+
+    # def removeDuplicates(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: int
+    #     """
+    #     i = 0 
+    #     for j in range(1, len(nums)):
+    #         if nums[i] != nums[j]:
+    #             i+=1
+    #             nums[i] = nums[j]
+       
+    #     return i+1
